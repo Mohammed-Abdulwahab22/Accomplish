c8dec46cb70a09faac27e886d60fef04454f0f90
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2024-05-03 10:06:25 +0300

    Adding Bottom navigation bar to navigate between the screens "icons and names should be changed later"

---
+++ accomplish-app/Screens/PersonalTasksScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function PersonalTasksScreen() {
+  return (
+    <View>
+      <Text>PersonalTasksScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
